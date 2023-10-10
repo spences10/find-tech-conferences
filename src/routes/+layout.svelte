9a2afc68f540c60c9cdb36577ee5bd4dcae03419
spences10 <spences10apps@gmail.com>
2023-10-10 09:41:47 +0100

    feat: :sparkles: add fathom client

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,22 @@
 <script>
+	import { browser } from '$app/environment';
+	import { page } from '$app/stores';
+	import {
+		PUBLIC_FATHOM_ID,
+		PUBLIC_FATHOM_URL,
+	} from '$env/static/public';
+	import * as Fathom from 'fathom-client';
+	import { onMount } from 'svelte';
 	import '../app.postcss';
+
+	onMount(() => {
+		Fathom.load(PUBLIC_FATHOM_ID, {
+			url: PUBLIC_FATHOM_URL,
+			excludedDomains: ['localhost'],
+		});
+	});
+
+	$: $page.url.pathname, browser && Fathom.trackPageview();
 </script>
 
 <slot />
