e667a730b867bd34fb1ac3677efe13575e8c9b38
spences10 <spences10apps@gmail.com>
2023-10-11 20:28:34 +0100

    feat: :sparkles: add input component

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,2 +1,3 @@
 export { default as ConferenceCard } from './conference-card.svelte';
+export { default as FormInput } from './form-input.svelte';
 export { default as Header } from './header.svelte';
