9f7d2a3349c80d47d195c7315e42992a65a24306
spences10 <spences10apps@gmail.com>
2024-07-20 20:25:25 +0100

    refactor: :recycle: remove drizzle related stuffs

--- src/lib/server/schema/tags.ts
+++
@@ -1,9 +0,0 @@
-import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
-import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
-
-export const tags = sqliteTable('tags', {
-	id: integer('id').primaryKey().primaryKey(),
-	tag_name: text('tag_name').notNull(),
-});
-export const insert_tag_schema = createInsertSchema(tags);
-export const select_tag_schema = createSelectSchema(tags);
